10f503d7516517b18a480a379aaba5dafb3467e3
Bridger Thompson <bridgerat00@gmail.com>
2023-10-04 07:20:03 -0600

    tailscale url

--- client/src/pages/shows/commentsApiService.ts
+++ client/src/pages/shows/commentsApiService.ts
@@ -5,14 +5,16 @@ export const commentsService = {
   async updateComments(comments: Comment[]) {
     console.log("adding", comments)
     // const url = "https://100.68.122.57:3003/api/store?key=showComments"
-    const url = "https://bridger-shows.duckdns.org:3003/api/store?key=showComments"
+    // const url = "https://bridger-shows.duckdns.org:3003/api/store?key=showComments"
+    const url = "https://100.68.122.57:3003/api/store?key=showComments"
     const response = await axios.post(url, comments)
     console.log(response, response.data)
     return response.data
   },
   async getComments(): Promise<Comment[]> {
     // const url = "https://100.68.122.57:3003/api/store?key=showComments"
-    const url = "https://bridger-shows.duckdns.org:3003/api/store?key=showComments"
+    // const url = "https://bridger-shows.duckdns.org:3003/api/store?key=showComments"
+    const url = "https://100.68.122.57:3003/api/store?key=showComments"
     const response = await axios.get(url)
     return response.data
   },
